31f2e7ee0e464d74305f8d536f7079af87b039de
Basirul Billah <basirulbillah@gmail.com>
2022-03-26 16:26:21 +0600

    added components

---
+++ src/component/Shop/Shop.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Cart from '../Cart/Cart';
+import Product from '../Product/Product';
+import './Shop.css'
+
+const Shop = () => {
+    return (
+        <div className="shop-container">
+            <div className="product-container">
+                <Product></Product>
+            </div>
+            <div className="cart-container">
+                <Cart></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
